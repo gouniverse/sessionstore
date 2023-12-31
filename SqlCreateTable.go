61b3a22e2092f00156b304b583995b196e3c3cbe
Milan Lesichkov <lesichkovm@gmail.com>
2023-12-31 09:19:48 +0000

    Updated dependencies

--- SqlCreateTable.go
+++ SqlCreateTable.go
@@ -1,83 +1,60 @@
 package sessionstore
 
-import "github.com/gouniverse/sql"
+import (
+	"github.com/gouniverse/sb"
+)
 
 // SQLCreateTable returns a SQL string for creating the cache table
 func (st *Store) SQLCreateTable() string {
-	sql := sql.NewBuilder(st.dbDriverName).
+	sql := sb.NewBuilder(st.dbDriverName).
 		Table(st.sessionTableName).
-		Column("id", sql.COLUMN_TYPE_STRING, map[string]string{
-			sql.COLUMN_ATTRIBUTE_PRIMARY: "yes",
-			sql.COLUMN_ATTRIBUTE_LENGTH:  "40",
+		Column(sb.Column{
+			Name:       "id",
+			Type:       sb.COLUMN_TYPE_STRING,
+			Length:     40,
+			PrimaryKey: true,
 		}).
-		Column("session_key", sql.COLUMN_TYPE_STRING, map[string]string{
-			sql.COLUMN_ATTRIBUTE_LENGTH: "255",
+		Column(sb.Column{
+			Name:   "session_key",
+			Type:   sb.COLUMN_TYPE_STRING,
+			Length: 255,
 		}).
-		Column("user_id", sql.COLUMN_TYPE_STRING, map[string]string{
-			sql.COLUMN_ATTRIBUTE_LENGTH: "40",
+		Column(sb.Column{
+			Name:   "user_id",
+			Type:   sb.COLUMN_TYPE_STRING,
+			Length: 40,
 		}).
-		Column("ip_address", sql.COLUMN_TYPE_STRING, map[string]string{
-			sql.COLUMN_ATTRIBUTE_LENGTH: "50",
+		Column(sb.Column{
+			Name:   "ip_address",
+			Type:   sb.COLUMN_TYPE_STRING,
+			Length: 50,
 		}).
-		Column("user_agent", sql.COLUMN_TYPE_STRING, map[string]string{
-			sql.COLUMN_ATTRIBUTE_LENGTH: "1024",
+		Column(sb.Column{
+			Name:   "user_agent",
+			Type:   sb.COLUMN_TYPE_STRING,
+			Length: 1024,
 		}).
-		Column("session_value", sql.COLUMN_TYPE_TEXT, map[string]string{}).
-		Column("expires_at", sql.COLUMN_TYPE_DATETIME, map[string]string{}).
-		Column("created_at", sql.COLUMN_TYPE_DATETIME, map[string]string{}).
-		Column("updated_at", sql.COLUMN_TYPE_DATETIME, map[string]string{}).
-		Column("deleted_at", sql.COLUMN_TYPE_DATETIME, map[string]string{}).
+		Column(sb.Column{
+			Name: "session_value",
+			Type: sb.COLUMN_TYPE_TEXT,
+		}).
+		Column(sb.Column{
+			Name: "expires_at",
+			Type: sb.COLUMN_TYPE_DATETIME,
+		}).
+		Column(sb.Column{
+			Name: "created_at",
+			Type: sb.COLUMN_TYPE_DATETIME,
+		}).
+		Column(sb.Column{
+			Name: "updated_at",
+			Type: sb.COLUMN_TYPE_DATETIME,
+		}).
+		Column(sb.Column{
+			Name: "deleted_at",
+			Type: sb.COLUMN_TYPE_DATETIME,
+		}).
 		CreateIfNotExists()
 
 	return sql
-
-	// sqlMysql := `
-	// CREATE TABLE IF NOT EXISTS ` + st.sessionTableName + ` (
-	//   id varchar(40) NOT NULL PRIMARY KEY,
-	//   session_key varchar(255) NOT NULL,
-	//   session_value text,
-	//   expires_at datetime,
-	//   created_at datetime NOT NULL,
-	//   updated_at datetime NOT NULL,
-	//   deleted_at datetime
-	// );
-	// `
-
-	// sqlPostgres := `
-	// CREATE TABLE IF NOT EXISTS "` + st.sessionTableName + `" (
-	//   "id" varchar(40) NOT NULL PRIMARY KEY,
-	//   "session_key" varchar(255) NOT NULL,
-	//   "session_value" text,
-	//   "expires_at" timestamptz(6),
-	//   "created_at" timestamptz(6) NOT NULL,
-	//   "updated_at" timestamptz(6) NOT NULL,
-	//   "deleted_at" timestamptz(6)
-	// )
-	// `
-
-	// sqlSqlite := `
-	// CREATE TABLE IF NOT EXISTS "` + st.sessionTableName + `" (
-	//   "id" varchar(40) NOT NULL PRIMARY KEY,
-	//   "session_key" varchar(255) NOT NULL,
-	//   "session_value" text,
-	//   "expires_at" datetime,
-	//   "created_at" datetime NOT NULL,
-	//   "updated_at" datetime NOT NULL,
-	//   "deleted_at" datetime
-	// )
-	// `
-
-	// sql := "unsupported driver " + st.dbDriverName
-
-	// if st.dbDriverName == "mysql" {
-	// 	sql = sqlMysql
-	// }
-	// if st.dbDriverName == "postgres" {
-	// 	sql = sqlPostgres
-	// }
-	// if st.dbDriverName == "sqlite" {
-	// 	sql = sqlSqlite
-	// }
-
-	// return sql
 }
--- go.mod
+++ go.mod
@@ -10,21 +10,10 @@ require (
 )
 
 require (
-	github.com/darkoatanasovski/htmltags v1.0.0 // indirect
-	github.com/emirpasic/gods v1.18.1 // indirect
-	github.com/gouniverse/api v1.5.0 // indirect
 	github.com/gouniverse/maputils v0.7.0 // indirect
-	github.com/gouniverse/sql v0.9.0 // indirect
-	github.com/gouniverse/utils v1.34.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
+	github.com/gouniverse/sb v0.1.0
 	github.com/samber/lo v1.39.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	github.com/tdewolff/minify/v2 v2.20.9 // indirect
-	github.com/tdewolff/parse/v2 v2.7.6 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 )
